b610e27f513b198ae6a2b431e5b1583908a445cd
Mary Lou D. Cortez <maryloucortez12@gmail.com>
2024-01-22 16:13:28 +0800

    first commit

---
+++ resources/views/livewire/contact-us.blade.php
@@ -0,0 +1,24 @@
+
+<div class="border-2 w-4/12 mx-auto">
+    <div class="mt-24">
+        <h1 class="text-center text-4xl font-extrabold">Contact Us</h1>
+        <div class="text-gray-600 flex justify-center mt-4">
+            <p class="w-[600px] text-center text-lg">Got a technical issue? Want to send feedback abouta beta feature? Need details about our Business plan? Let us know.</p>
+        </div>
+    </div>
+
+    <div class="mt-16">
+        <div>
+            <label for="email" class="block font-semibold">Your email</label>
+            <input type="text" class="border border-gray-300 block w-full rounded-md">
+        </div>
+        <div class="mt-5">
+            <label for="subject" class="block font-semibold">Subject</label>
+            <input type="text" class="border border-gray-300 block w-full rounded-md">
+        </div>
+        <div class="mt-5">
+            <label for="message" class="block font-semibold">Your message</label>
+            <input type="text">
+        </div>
+    </div>
+</div>
